440c78f80c7f9249489e3963d008dc283ec38b68
yuey4n9 <mmdyueyang@gmail.com>
2025-08-07 14:24:31 +0800

    _time.Parse

--- time/time.go
+++ time/time.go
@@ -1,5 +1,27 @@
-package time
+package _time
+
+import (
+	"errors"
+	"strconv"
+	"time"
+)
 
 func IsLeapYear(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
+
+func ParseMillisecondTimestamp(ts string) (time.Time, error) {
+	timestamp, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
+		return time.Time{}, errors.New("无效的毫秒时间戳格式: " + err.Error())
+	}
+	return time.Unix(0, timestamp*int64(time.Millisecond)), nil
+}
+
+func ParseSecondTimestamp(ts string) (time.Time, error) {
+	timestamp, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
+		return time.Time{}, errors.New("无效的秒时间戳格式: " + err.Error())
+	}
+	return time.Unix(timestamp, 0), nil
 }
